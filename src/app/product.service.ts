922b0569ac297edeaac5f9b5a38567505aa76a44
kasmaj11 <kasmaj@poczta.onet.pl>
2018-06-17 22:52:37 +0200

    module 6

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -2,13 +2,14 @@ import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
 import 'rxjs/add/operator/map';
 import { Album } from './album';
-import { Track } from './track';
 import { Observable } from 'rxjs/Observable';
+import { Product } from './product';
 
 @Injectable()
 export class ProductService {
 
   private _albumUrl: string = "../assets/album.json";
+  private _productsUrl: string = "../assets/products.json";
 
   constructor(private _http: Http) { }
 
@@ -17,4 +18,8 @@ export class ProductService {
     .map((response) => <Album>response.json());
   }
 
+  getProducts(): Observable<Product[]> {
+    return this._http.get(this._productsUrl)
+      .map((response) => <Product[]>response.json());
+  }
 }
